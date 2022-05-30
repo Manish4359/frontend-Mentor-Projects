52877e3ad0dab7f49f43ba85cf91a8fffda8b6df
Manish4359 <manishkr.official4359@gmail.com>
2022-05-30 14:45:38 +0530

    created responsive page

--- interactive-comments-section-main/script.js
+++ interactive-comments-section-main/script.js
@@ -13,7 +13,6 @@ xhttp.onload = () => {
   if (xhttp.status === 200 && xhttp.readyState === 4) {
 
     userData = JSON.parse(xhttp.responseText);
-    console.log(userData);
 
     currentUser = userData.currentUser;
 
@@ -25,17 +24,7 @@ xhttp.onload = () => {
       const userCommentMarkup = `
           <div class="user-comment">
             ${html}
-          </div>
-          `
-      const userReplyMarkup = `
-          <div class="user-replys">
-          <div class="bar">qwerty</div>
-            <div class="user-comment reply">
-                ${html}
-            </div>
-          </div>
-          `
-
+          </div>`;
 
       const commentCont = document.createElement('div');
 
@@ -48,8 +37,7 @@ xhttp.onload = () => {
 
         comment.replies.forEach((reply) => {
 
-          console.log(comment.user.username)
-          html = commentMarkup(reply, currentUser.username,comment.user.username);
+          html = commentMarkup(reply, currentUser.username, comment.user.username);
 
           replys.classList.add('user-replys');
 
@@ -87,11 +75,6 @@ xhttp.onload = () => {
 
       e.target.removeEventListener('click', event)
     })
-    /*
-    commentActions.forEach(actions => {
-
-    })
-    */
 
   }
 
@@ -99,7 +82,7 @@ xhttp.onload = () => {
 xhttp.open('GET', './data.json')
 xhttp.send();
 
-function commentMarkup({ content, createdAt, score, user },currentUser,replyToUser) {
+function commentMarkup({ content, createdAt, score, user }, currentUser, replyToUser) {
   const { username, image } = user;
 
   return `
@@ -109,8 +92,7 @@ function commentMarkup({ content, createdAt, score, user },currentUser,replyToUs
       <img src="./images/icon-minus.svg" alt="downvote" class="downvote">
 
     </div>
-    <div class="comment-details">
-
+    
       <div class="comment-heading">
         <div class="user">
           <img src="${image.png}" alt="user">
@@ -119,8 +101,9 @@ function commentMarkup({ content, createdAt, score, user },currentUser,replyToUs
         ${currentUser === username ? '<div id=you>You</div>' : ''}
         <div class="comment-date">
           ${createdAt}
-        </div>
-        <div class="comment-actions" >
+          </div>
+      </div>
+      <div class="comment-actions" >
           <div class="comment-reply" style=display:${currentUser === username ? 'none' : 'block'}>
           <img src="./images/icon-reply.svg" alt="reply">
             Reply
@@ -137,46 +120,45 @@ function commentMarkup({ content, createdAt, score, user },currentUser,replyToUs
 
         </div>
 
-      </div>
       <p class="comment-description">
-      ${replyToUser?`<span style=color:blue>${'@'+replyToUser} </span>`:''}${content}
+        ${replyToUser ? `<span style=color:blue>${'@' + replyToUser} </span>` : ''}${content}
       </p>
-      ${currentUser === username ? '<div class=comment-update style=display:none >Update</div>' : ''}
-    </div>`
+        ${currentUser === username ? '<div class=comment-update style=display:none >Update</div>' : ''}
+    `
 }
 
 function commentEvents(elem) {
 
-  console.log(elem)
 
   if (elem.classList.contains('comment-edit') || elem.closest('.comment-edit')) {
 
+    const parentComment = elem.closest('.user-comment');
+
+    const commentAction = parentComment.querySelector('.comment-actions');
+    const commentUpdate = parentComment.querySelector('.comment-update');
+    let commentDesc = parentComment.querySelector('.comment-description');
+    const replyTo = commentDesc.innerText.split(" ")[0];
 
 
-    //console.log(e.target.closest('.comment-details').children[1])
-    const commentUpdate = elem.closest('.comment-details').children[2];
-    let commentDesc = elem.closest('.comment-details').children[1];
-    const replyTo=commentDesc.innerText.split(" ")[0];
-
-
-    commentDesc.textContent=commentDesc.textContent.replace(replyTo,"");
+    commentDesc.textContent = commentDesc.textContent.replace(replyTo, "");
 
     commentDesc.toggleAttribute('contenteditable');
-    commentDesc.style.border = '2px solid black';
+    commentDesc.style.border = '2px solid hsl(238, 40%, 52%)';
     commentUpdate.style.display = 'block';
 
-    elem.closest('.comment-heading').querySelector('.comment-edit').style.display='none';
-    updateComment(commentDesc, commentUpdate,elem.closest('.comment-heading').querySelector('.comment-edit'),replyTo);
+    commentAction.querySelector('.comment-edit').style.display = 'none';
+    updateComment(commentDesc, commentUpdate, commentAction.querySelector('.comment-edit'), replyTo);
 
   }
 
   if (elem.classList.contains('comment-reply') || elem.closest('.comment-reply')) {
 
-    const parentHeading=elem.closest('.comment-heading');
+    const parentComment = elem.closest('.user-comment');
 
-    console.log(elem.closest('.comment-heading').querySelector('.username').textContent)
 
-    const replyToUser=parentHeading.querySelector('.username').innerText;
+
+
+    const replyToUser = parentComment.querySelector('.username').innerText;
 
     const currentUserReplyMarkup = `
         <div class="currentuser">
@@ -185,18 +167,17 @@ function commentEvents(elem) {
         <div class="currentuser-reply">Reply</div>
         </div>`
 
-    elem.closest('.user-comment').insertAdjacentHTML('afterend', currentUserReplyMarkup);
+    parentComment.insertAdjacentHTML('afterend', currentUserReplyMarkup);
 
-    parentHeading.querySelector('.comment-reply').style.display='none';
+    parentComment.querySelector('.comment-reply').style.display = 'none';
 
 
-    replyComment(elem.closest('.user-comment'),parentHeading.querySelector('.comment-reply'),replyToUser);
+    replyComment(parentComment, parentComment.querySelector('.comment-reply'), replyToUser);
 
   }
 
 
   if (elem.classList.contains('comment-delete') || elem.closest('.comment-delete')) {
-    console.log('delete');
 
     deleteModal.style.display = 'grid';
 
@@ -215,11 +196,11 @@ function commentEvents(elem) {
 
 
   if (elem.classList.contains('comment-send')) {
-    console.log('send');
 
-    console.log(elem.closest('.currentuser').querySelector('.currentuser-comment').value);
+    const parentComment = elem.closest('.currentuser');
+    const commentContent = parentComment.querySelector('.currentuser-comment').value;
 
-    const commentContent = elem.closest('.currentuser').querySelector('.currentuser-comment').value;
+    parentComment.querySelector('.currentuser-comment').value = '';
 
     const data = {
       content: commentContent,
@@ -232,29 +213,27 @@ function commentEvents(elem) {
     ${commentMarkup(data, currentUser.username)}
     </div>
   `
-    if (commentContent === '') {
-      return;
+    if (commentContent !== '') {
+      parentComment.insertAdjacentHTML('beforebegin', markup)
     }
-    elem.closest('.currentuser').insertAdjacentHTML('beforebegin', markup)
   }
 }
 
 
 
 
-function replyComment(elem,replyBtn,replyToUser) {
+function replyComment(elem, replyBtn, replyToUser) {
 
-  console.log(replyBtn)
 
   elem.nextElementSibling.addEventListener('click', function send(e) {
-    console.log(e.target);
-    replyBtn.style.display='block';
 
     if (e.target.classList.contains('currentuser-reply')) {
       let commentContent = e.currentTarget.querySelector('.currentuser-comment').value;
-      
+
       if (commentContent === '') {
         e.currentTarget.remove();
+        replyBtn.style.display = 'block';
+
         return;
       }
 
@@ -266,13 +245,13 @@ function replyComment(elem,replyBtn,replyToUser) {
 
       }
       const markup = `<div class="user-comment reply" style='align-items:stretch'>
-      ${commentMarkup(data, currentUser.username,replyToUser)}
+      ${commentMarkup(data, currentUser.username, replyToUser)}
       </div>
 
     `
       elem.insertAdjacentHTML('afterend', markup);
-
       e.currentTarget.remove();
+      replyBtn.style.display = 'block';
 
     }
   })
@@ -285,11 +264,9 @@ function replyComment(elem,replyBtn,replyToUser) {
 function deleteComment(elem) {
 
   document.querySelector('.confirm-delete').addEventListener('click', function deleteElem(e) {
-    console.log(e.target)
 
     if (e.target.classList.contains('delete-yes')) {
 
-      console.log(elem)
       elem.remove();
     }
     deleteModal.style.display = '';
@@ -306,21 +283,26 @@ function deleteComment(elem) {
 
 
 
-function updateComment(elem, updateBtn,replyBtn,replyTo) {
+function updateComment(elem, updateBtn, replyBtn, replyTo) {
 
   updateBtn.addEventListener('click', function update(e) {
 
-    console.log(replyTo);
 
 
     elem.toggleAttribute('contenteditable');
     elem.style.border = '2px solid transparent';
 
+    if (elem.innerText === '') {
+      elem.closest('.user-comment').remove();
+      return;
+
+    }
+
     elem.innerHTML = `<span style=color:blue>${replyTo} </span>${elem.textContent}`
 
     updateBtn.style.display = 'none';
-    replyBtn.style.display='block';
-
+    replyBtn.style.display = 'block';
     updateBtn.removeEventListener('click', update);
+
   })
 }
